a3509a87ea1347a4fe8688ed1c5792bfc41930fb
Thales Garcia <thalesgsoares@gmail.com>
2022-01-24 15:11:16 -0300

    addEventListener, createElement and appendChild

--- js/principal.js
+++ js/principal.js
@@ -41,9 +41,47 @@ for (i=0; i<pacientes.length; i++) {
     if (pesoValido == false && alturaValido == false){
         tdImc.textContent = "#";
     }
-
-    
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+    
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = peso/(altura*altura);
+
+    //colocando as td dentro da tr 
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr)
+    
+});
+
+
 
 
 
