48d2f0b54a261a5efdd6ac3330a17943b3a6c62f
thales-garcia <thalesgsoares@gmail.com>
2022-02-21 21:00:21 -0300

    Searching patients with AJAX XMLHttpRequest

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,31 @@
+var botaoBuscar = document.querySelector("#buscar-paciente");
+
+botaoBuscar.addEventListener("click", function(){
+    console.log("Buscando pacientes...");
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function(){
+
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if (xhr.status == 200){
+            erroAjax.classList.add("invisivel")
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+    
+            pacientes.forEach(function(paciente){
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("invisivel")
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }
+    });
+
+    xhr.send();
+})
+
--- js/form.js
+++ js/form.js
@@ -7,8 +7,6 @@ botaoAdicionar.addEventListener("click", function(event){
 
     var paciente = obtemPacienteDoFormulario(form);
 
-    var pacienteTr = montaTr(paciente);
-
     var erros = validaPaciente(paciente);
 
     if (erros.length>0){
@@ -17,14 +15,18 @@ botaoAdicionar.addEventListener("click", function(event){
         return
     }
 
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
+    adicionaPacienteNaTabela(paciente);
 
     form.reset();
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente){
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function obtemPacienteDoFormulario(form){
 
